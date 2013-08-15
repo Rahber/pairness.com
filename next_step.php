89e9f80ebaf1f86da4163aac2e9cc445007a317b
zubairze7 <muhammadzubair107@gmail.com>
2013-08-15 12:05:52 +0500

    next_step.php

---
+++ next_step.php
@@ -0,0 +1,18 @@
+<?php
+/*********************************************
+
+	Copyright:	Cozmuler Pakistan
+	File Name:	candidate.php
+	Package:	Pairness.com
+	Author:		Rahber
+
+*********************************************/
+include('./includes/functions.php');
+include('./assets/template/header.coz');
+include('./assets/template/candidate.coz');
+include('./assets/template/next_step.coz');
+include('./assets/template/footer.coz');
+
+
+
+?>
